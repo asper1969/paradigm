0f6393585d8bd510a0f2fb53f5518cd4427beeb4
asper1969 <aspiosa@gmail.com>
2017-12-06 18:00:07 +0600

    06122017

--- front-page.php
+++ front-page.php
@@ -1,7 +1,41 @@
-<?php get_header(); ?>
+<?php
+$lang = pll_current_language();
+$args = array(
+    'category' => 18
+);
+$slides = get_posts($args);
+
+get_header(); ?>
+<ul><?php pll_the_languages();?></ul>
+<pre>
+    <?php echo $lang;?>
+</pre>
 <div class="slider">
 <!--TODO: slider-->
-    #SLIDER#
+    <?php foreach($slides as $slide): ?>
+    <pre>
+        <?php
+        $slide_id = $slide->ID;
+        $slide_meta = get_post_meta($slide_id);
+        $slide_img_id = $slide_meta['slide'][0];
+        $slide_img = fly_get_attachment_image_src($slide_img_id, array(1920, 600))['src'];
+        print_r($slide);
+        ?>
+    </pre>
+    <div class="slide" style="background: url(<?php echo $slide_img;?>); background-size: cover;">
+        <a href="<?php echo $slide_meta['more'][0];?>" class="more">
+            <?php
+            if($lang == 'ru'){
+                echo 'Подробнее';
+            }elseif($lang == 'kk'){
+                echo 'Подробнее kz';
+            }else{
+                echo 'More';
+            }
+            ?>
+        </a>
+    </div>
+    <?php endforeach;?>
 </div>
 <div class="portfolio">
     #PORTFOLIO#
--- function.php
+++ function.php
@@ -2,4 +2,10 @@
 require_once ( get_stylesheet_directory() . '/theme-options.php' );
 
 add_theme_support('post-thumbnails');
-add_image_size('Slide', 1920, 600, true);
+//add_image_size('slide', 1920, 600, true);
+
+if ( function_exists( 'fly_add_image_size' ) ) {
+    fly_add_image_size( 'slider', 1920, 600, true );
+    fly_add_image_size( 'home_page_square_2x', 1000, 1000, true );
+    fly_add_image_size( 'cropped_top_left', 1000, 1000, array( 'left', 'top' ) );
+}
