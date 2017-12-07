d88b32f6a76e7b82f796e614db340e9c59d01da0
asper1969 <aspiosa@gmail.com>
2017-12-07 17:51:47 +0600

    07122017

--- front-page.php
+++ front-page.php
@@ -1,39 +1,40 @@
 <?php
 $lang = pll_current_language();
 $args = array(
-    'category' => 18
+    'category' => pll_get_term(18)
 );
 $slides = get_posts($args);
 
 get_header(); ?>
 <ul><?php pll_the_languages();?></ul>
-<pre>
-    <?php echo $lang;?>
-</pre>
 <div class="slider">
-<!--TODO: slider-->
     <?php foreach($slides as $slide): ?>
-    <pre>
-        <?php
-        $slide_id = $slide->ID;
-        $slide_meta = get_post_meta($slide_id);
-        $slide_img_id = $slide_meta['slide'][0];
-        $slide_img = fly_get_attachment_image_src($slide_img_id, array(1920, 600))['src'];
-        print_r($slide);
-        ?>
-    </pre>
+    <?php
+    $slide_id = $slide->ID;
+    $slide_meta = get_post_meta($slide_id);
+    $slide_img_id = $slide_meta['slide'][0];
+    $slide_img = fly_get_attachment_image_src($slide_img_id, array(1920, 600))['src'];
+    ?>
     <div class="slide" style="background: url(<?php echo $slide_img;?>); background-size: cover;">
-        <a href="<?php echo $slide_meta['more'][0];?>" class="more">
-            <?php
-            if($lang == 'ru'){
-                echo 'Подробнее';
-            }elseif($lang == 'kk'){
-                echo 'Подробнее kz';
-            }else{
-                echo 'More';
-            }
-            ?>
-        </a>
+        <div class="container">
+            <div class="text">
+                <?php echo $slide->post_title;?>
+            </div>
+            <div class="text">
+                <?php echo $slide->post_content;?>
+            </div>
+            <a href="<?php echo $slide_meta['more'][0];?>" class="more">
+                <?php
+                if($lang == 'ru'){
+                    echo 'Подробнее';
+                }elseif($lang == 'kk'){
+                    echo 'Подробнее kz';
+                }else{
+                    echo 'More';
+                }
+                ?>
+            </a>
+        </div>
     </div>
     <?php endforeach;?>
 </div>
